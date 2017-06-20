fca59c494e584c8946026effec0f3ddf0084ec18
DAVIDE VAGHETTI <davide.vaghetti@unipi.it>
2017-06-20 17:52:57 +0200

    PHP/OpenIDConnectClient + Silex

--- oidc-php-rp/oidc_rp/index.php
+++ oidc-php-rp/oidc_rp/index.php
@@ -10,10 +10,15 @@ $app->register(new Silex\Provider\TwigServiceProvider(),
 		     'twig.path' => __DIR__.'/templates',
 		     ));
 
+$app->register(new Silex\Provider\SessionServiceProvider());
+
 $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Hello '.$app->escape($name);
   });
 
+$app['oidc-rp'] = function() {
+  return new OpenIDConnectClient('https://mitreid.org/');
+};
 
 $app->get('/twighello/{name}', function ($name) use ($app) {
     return $app['twig']->render('twighello.html', 
@@ -22,6 +27,32 @@ $app->get('/twighello/{name}', function ($name) use ($app) {
 				      ));
   });
 
+$app->get('/oidc-rp', function () use ($app) {
+    $oidc = $app['oidc-rp'];
+    if (null === $oidcclient = $app['session']->get('oidcclient')) {
+      $oidc->register();
+      $app['session']-> set('oidcclient', array('client_id' => $oidc->getClientID(),
+					      'client_secret' => $oidc->getClientSecret()));
+      return $app->redirect('/oidc-rp');
+    } 
+    $oidc->setClientID($oidcclient['client_id']);
+    $oidc->setClientSecret($oidcclient['client_secret']);
+    $oidc->authenticate();
+    $name = $oidc->requestUserInfo('sub');
+    return 'Hello '.$app->escape($name);
+  });
+
+/**
+$app->get('/oidc-rp', function () use ($app) {
+    $oidcclient = $app['session']->get('oidcclient');
+
+    $oidc = $app['oidc-rp'];
+
+  });
+*/
+
 $app['debug'] = true;
+
+
 
 $app->run();
